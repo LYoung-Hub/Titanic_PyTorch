6f9ed67e3932b2099bbed634a0d946664270b534
ling <zhilingzhouzju@gmail.com>
2020-03-24 14:34:59 -0700

    add fare function

--- data_process.py
+++ data_process.py
@@ -37,6 +37,10 @@ class PreProcess(object):
     def process_Pclass(self):
         return pd.get_dummies(self.data['Pclass'])
 
+    def process_fare(self):
+        fare_bins = pd.qcut(self.data['Fare'], 4, labels=[0, 1, 2, 3])
+        return pd.get_dummies(fare_bins)
+
     def process_embarked(self):
         return pd.get_dummies(self.data['Embarked'])
 
